7487c515af8d8d0bacf0a202c7d88db9ea3b5708
Priyank Tejani <46929256+priyanktejani@users.noreply.github.com>
2022-06-06 16:58:06 +0100

    Add fiftygram

---
+++ fiftygram/app/src/main/java/edu/harvard/cs50/fiftygram/MainActivity.java
@@ -0,0 +1,157 @@
+package edu.harvard.cs50.fiftygram;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+import jp.wasabeef.glide.transformations.gpu.InvertFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.PixelationFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.SepiaFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.SketchFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.ToonFilterTransformation;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+    private ImageView imageView;
+    private Bitmap original;
+    private int PERMISSION_CODE = 1;
+    // File name without Extension
+    private String fileWOExtension;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+    }
+
+    public void apply(Transformation<Bitmap> filter) {
+        if (original != null) {
+            Glide
+                    .with(this)
+                    .load(original)
+                    .apply(RequestOptions.bitmapTransform(filter))
+                    .into(imageView);
+        }
+    }
+
+    public void applySepia(View view) {
+        apply(new SepiaFilterTransformation());
+    }
+
+    public void applyToon(View view) {
+        apply(new ToonFilterTransformation());
+    }
+
+    public void applySketch(View view) {
+        apply(new SketchFilterTransformation());
+    }
+
+    public void applyInvert(View view) {
+        apply(new InvertFilterTransformation());
+    }
+
+    public void applyPixelation(View view) {
+        apply(new PixelationFilterTransformation());
+    }
+
+    public void choosePhoto(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, 1);
+        } else {
+            // Ask for Permission if not Granted
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
+        }
+    }
+
+    public void savePhoto(View view) {
+        try {
+            // Get filtered Bitmap from Image View
+            Bitmap filtered = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            MediaStore.Images.Media.insertImage(getContentResolver(), filtered, "Fiftygram_" + fileWOExtension, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBitmapPathFromURI(Uri contentUri) {
+        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null
+                , MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("", "PERMISSION GRANTED");
+            } else {
+                Log.d("", "PERMISSION DENIED");
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                Uri uri = data.getData();
+
+                // Get Bitmap file name from uri
+                String path = getBitmapPathFromURI(uri);
+                String filename = path.substring(path.lastIndexOf("/") + 1);
+                if (filename.indexOf(".") > 0) {
+                    fileWOExtension = filename.substring(0, filename.lastIndexOf("."));
+                } else {
+                    fileWOExtension = filename;
+                }
+
+                ParcelFileDescriptor parcelFileDescriptor =
+                        getContentResolver().openFileDescriptor(uri, "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                original = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                imageView.setImageBitmap(original);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
